3dc50534f5150ff113e7eb03c7828bb49a6927cf
Fumeta-Imperial <108669451+Fumeta-Imperial@users.noreply.github.com>
2024-07-22 01:09:09 -0300

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -11,11 +11,11 @@ logging.basicConfig(
 
 class Config(object):
     
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7281039003:AAFqm0kIovUHekX2sveJPKSlJL3BWve41sQ")
     
-    API_ID = int(os.environ.get("API_ID", ""))
+    API_ID = int(os.environ.get("API_ID", "15968596"))
     
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "1845e487ac8dbcdeb5b3a3f0bdd1a1a5")
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     
@@ -43,13 +43,13 @@ class Config(object):
     
     SESSION_NAME = os.environ.get("SESSION_NAME", "UploadLinkToFileBot")
     
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1692017446"))
     
     LOGGER = logging
 
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+    #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1692017446")
     
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "5177147562"))
     
     TG_MIN_FILE_SIZE = 2097152000
     
